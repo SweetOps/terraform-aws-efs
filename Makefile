4cc676b6af486a1059d83b781a5f75a339146106
SweetOps <vladimir.fess@gmail.com>
2018-11-16 16:48:17 +0200

    update

--- Makefile
+++ Makefile
@@ -1,17 +1,6 @@
 SHELL := /bin/bash
 
-<<<<<<< HEAD
 -include $(shell curl -sSL -o .build-harness "https://git.io/build-harness"; echo .build-harness)
 
 lint:
 	$(SELF) terraform:install terraform:get-modules terraform:get-plugins terraform:lint terraform:validate
-=======
-# List of targets the `readme` target should call before generating the readme
-export README_DEPS ?= docs/targets.md docs/terraform.md
-
--include $(shell curl -sSL -o .build-harness "https://git.io/build-harness"; echo .build-harness)
-
-## Lint terraform code
-lint:
-	$(SELF) terraform/install terraform/get-modules terraform/get-plugins terraform/lint terraform/validate
->>>>>>> master
--- variables.tf
+++ variables.tf
@@ -4,7 +4,6 @@ variable "name" {
 }
 
 variable "namespace" {
-<<<<<<< HEAD
   description = "Namespace (e.g. `cp` or `cloudposse`)"
   type        = "string"
 }
@@ -79,63 +78,4 @@ variable "ttl" {
 variable "enabled" {
   default     = "true"
   description = "Set to false to prevent the module from creating anything"
-=======
-  default     = "global"
-  description = "Namespace (_e.g._ `cp` or `cloudposse`)"
-}
-
-variable "stage" {
-  default     = "default"
-  description = "Stage (_e.g._ `prod`, `dev`, `staging`)"
-}
-
-variable "name" {
-  default     = "app"
-  description = "Name (_e.g._ `app` or `wordpress`)"
-}
-
-variable "security_groups" {
-  type        = "list"
-  description = "AWS security group IDs to allow to connect to the EFS"
-}
-
-variable "vpc_id" {
-  description = "AWS VPC ID"
-}
-
-variable "aws_region" {
-  description = "AWS region ID"
-}
-
-variable "subnets" {
-  type        = "list"
-  description = "AWS subnet IDs"
-}
-
-variable "availability_zones" {
-  type        = "list"
-  description = "Availability Zone IDs"
-}
-
-variable "zone_id" {
-  description = "Route53 dns zone ID"
-}
-
-variable "delimiter" {
-  type        = "string"
-  default     = "-"
-  description = "Delimiter to be used between `name`, `namespace`, `stage`, etc."
-}
-
-variable "attributes" {
-  type        = "list"
-  default     = []
-  description = "Additional attributes (e.g. `policy` or `role`)"
-}
-
-variable "tags" {
-  type        = "map"
-  default     = {}
-  description = "Additional tags (e.g. `map('BusinessUnit','XYZ')`)"
->>>>>>> master
 }
